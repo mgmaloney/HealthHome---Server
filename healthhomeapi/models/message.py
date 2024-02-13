2a12fb9ebda6bcf43c9da507b6b0797963e2d1c6
Matthew <matthewmaloneydev@gmail.com>
2024-02-13 16:39:42 -0600

    Message model updated. create and get_user_messages added to messages view

--- healthhomeapi/models/message.py
+++ healthhomeapi/models/message.py
@@ -1,5 +1,8 @@
 from django.db import models
+from .user import User
 
 class Message(models.Model):
     content = models.CharField(max_length=2000, default='')
     datetime = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL)
+    receiver = models.ForeignKey(User, on_delete=models.SET_NULL)
--- healthhomeapi/models/user_message.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from .user import User
-from .message import Message
-
-class User_Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
-    message = models.ForeignKey(Message, on_delete=models.SET_NULL)
